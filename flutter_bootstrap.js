3c353469bfb4d30a66c526555788a64054400f6c
Nitish Kumar <netyamshah52364@gmail.com>
2025-02-02 16:49:28 +0530

    file paths changed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "166107181"
+    serviceWorkerVersion: "4074283021"
   }
 });
